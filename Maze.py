216dffe6aadd33a386a35ede34bd19a4f991f7a6
ADABRAMS <67923657+ADABRAMS@users.noreply.github.com>
2020-08-11 11:49:32 -0400

    Add files via upload
    git push via pycharm not responding as expected, manually adding the Maze files back to github.

---
+++ Maze.py
@@ -0,0 +1,159 @@
+import turtle
+import random
+
+SIZE = 400
+BLOCKSIZE = 20
+
+EAST = 0
+NORTH = 1
+WEST = 2
+SOUTH = 3
+
+
+class Maze:
+    """ This class creates a random maze """
+
+    def __init__(self):
+        self.matrix = [[1 for i in range(int(SIZE / BLOCKSIZE))] for i in range(int(SIZE / BLOCKSIZE))]
+        self.turtle = turtle.Turtle()
+        self.s = turtle.Screen()
+        self.reset()
+
+    def reset(self):
+        self.s.bgcolor('blue')
+        self.turtle.penup()
+        self.s.setup(width=SIZE * 1.3, height=SIZE * 1.3)
+        self.turtle.goto(-(SIZE / 2 - BLOCKSIZE), SIZE / 2 - BLOCKSIZE)
+        self.turtle.shape('square')
+        self.turtle.color('white')
+        self.turtle.stamp()
+        self.matrix[0][0] = 0
+        self.depth = 0
+        self.goal = (-SIZE/2 - BLOCKSIZE, SIZE/2)
+
+    def fourcorners(self):
+        self.turtle.goto(-SIZE/2 - BLOCKSIZE, SIZE/2)
+        self.turtle.stamp()
+        self.turtle.goto(-SIZE/2, -SIZE/2)
+        self.turtle.stamp()
+        self.turtle.goto(SIZE/2, -SIZE/2)
+        self.turtle.stamp()
+        self.turtle.goto(SIZE/2, SIZE/2)
+        self.turtle.stamp()
+
+    def dig(self, d):
+        p = self.turtle.pos()
+        # (-180,180) for instance
+        if d == EAST:
+            newpos = (p[0] + BLOCKSIZE, p[1])
+            i0, j0 = self.pos2index(p)
+            matrix_value = self.matrix[i0 + 2][j0]
+            if matrix_value == 0:
+                return p
+            self.turtle.goto(newpos)
+            self.turtle.stamp()
+            i, j = self.pos2index(newpos)
+            self.matrix[i][j] = 0
+        if d == WEST:
+            if p[0] == -SIZE/2 - BLOCKSIZE:
+                return p
+            else:
+                newpos = (p[0] - BLOCKSIZE, p[1])
+                i0, j0 = self.pos2index(p)
+                matrix_value = self.matrix[i0 - 2][j0]
+                if matrix_value == 0:
+                    return p
+                self.turtle.goto(newpos)
+                self.turtle.stamp()
+                i, j = self.pos2index(newpos)
+                self.matrix[i][j] = 0
+        if d == NORTH:
+            if p[1] == SIZE/2 - BLOCKSIZE:
+                return p
+            else:
+                newpos = (p[0], p[1] + BLOCKSIZE)
+                i0, j0 = self.pos2index(p)
+                matrix_value = self.matrix[i0][j0 - 2]
+                if matrix_value == 0:
+                    return p
+                self.turtle.goto(newpos)
+                self.turtle.stamp()
+                i, j = self.pos2index(newpos)
+                self.matrix[i][j] = 0
+        if d == SOUTH:
+            if p[1] == -SIZE/2:
+                return p
+            else:
+                newpos = (p[0], p[1] - BLOCKSIZE)
+                i0, j0 = self.pos2index(p)
+                matrix_value = self.matrix[i0][j0 + 2]
+                if matrix_value == 0:
+                    return p
+                self.turtle.goto(newpos)
+                self.turtle.stamp()
+                i, j = self.pos2index(newpos)
+                self.matrix[i][j] = 0
+
+    def pos2index(self, p):
+        """ converts tuple p into tuple idx
+        200,-200 == 19,19
+        -180,180 == 0,0
+        -180,-200 == 0,19
+        200,180 == 19,0 """
+
+        i = int((p[0] + SIZE/2 - BLOCKSIZE) / BLOCKSIZE)
+        j = int((SIZE/2 - BLOCKSIZE - p[1]) / BLOCKSIZE)
+        return i, j
+
+    def getMatrixValueAt(self, pos):
+        x = int((pos[0] + SIZE/2 - BLOCKSIZE) / BLOCKSIZE)
+        y = int((SIZE/2 - BLOCKSIZE - pos[1]) / BLOCKSIZE)
+        if x < 0 or x > 19 or y < 0 or y > 19:
+            return -1
+        v = self.matrix[x][y]
+        return v
+
+        # take (-180,180) and convert to [0][0] v = self.matrix[0][0]
+        """ returns value of matrix at position pos """
+        pass
+
+    def setMatrixValueAt(self, pos, value):
+        x = int((pos[0] + 180) / BLOCKSIZE)
+        y = int((pos[1] - 180) / BLOCKSIZE)
+        try:
+            self.matrix[y][x] = value
+        except():
+            return False
+        if value == 0:
+            self.turtle.color('white')
+            self.turtle.stamp()
+        if value == 1:
+            self.turtle.color('blue')
+            self.turtle.stamp()
+        return True
+
+    def neighbors(self):
+        p = self.turtle.position()
+        r = []
+        r.append((self.getMatrixValueAt((p[0] + 40, p[1])), EAST))
+        r.append((self.getMatrixValueAt((p[0], p[1] + 40)), NORTH))
+        r.append((self.getMatrixValueAt((p[0] - 40, p[1])), WEST))
+        r.append((self.getMatrixValueAt((p[0], p[1] - 40)), SOUTH))
+        return r
+
+    def makeMaze(self):
+        n = self.neighbors()
+        oldpos = self.turtle.position()
+        while len(n) > 0:
+            nchoice = random.choice(n)
+            n.remove(nchoice)
+            self.turtle.goto(oldpos)
+            if nchoice[0] == 1:
+                d = nchoice[1]
+                self.dig(d)
+                self.dig(d)
+                if self.turtle.pos()[0] > self.depth:
+                    self.goal = self.turtle.pos()
+                    self.depth = self.goal[1]
+                self.depth = self.depth + 1
+                self.makeMaze()
---
+++ MazeTests.py
@@ -0,0 +1,129 @@
+import unittest
+import turtle
+from Maze import Maze
+
+SIZE = 400
+BLOCKSIZE = 20
+
+EAST = 0
+NORTH = 1
+WEST = 2
+SOUTH = 3
+
+
+class MazeTests(unittest.TestCase):
+    def setUp(self):
+        self.m = Maze()
+
+    def testScreen(self):
+        self.assertTrue(type(self.m.s) == turtle._Screen, "No Screen!")
+
+    def testTurtle(self):
+        self.assertTrue(type(self.m.turtle) == turtle.Turtle)
+
+    def testBackground(self):
+        self.assertTrue(self.m.s.bgcolor() == 'blue')
+
+    def testSize(self):
+        self.assertTrue(self.m.s.window_height() == SIZE * 1.3, f"window height is {self.m.s.window_height()}")
+        self.assertTrue(self.m.s.window_width() == SIZE * 1.3)
+
+    def testMatrixSize(self):
+        self.assertTrue(len(self.m.matrix) == SIZE / BLOCKSIZE)
+
+    def testReset(self):
+        self.m.reset()
+        self.assertTrue(self.m.turtle.pos() == (-(SIZE / 2 - BLOCKSIZE), SIZE / 2 - BLOCKSIZE))
+
+    def testCoordinates(self):
+        self.m.turtle.goto((-(SIZE / 2 - BLOCKSIZE)), (SIZE / 2 - BLOCKSIZE))
+        self.m.turtle.stamp()
+        self.m.turtle.goto((-(SIZE / 2 - BLOCKSIZE)), (-(SIZE / 2)))
+        self.m.turtle.stamp()
+        self.m.turtle.goto((SIZE / 2), (-(SIZE / 2)))
+        self.m.turtle.stamp()
+        self.m.turtle.goto((SIZE / 2), (SIZE / 2 - BLOCKSIZE))
+        self.m.turtle.stamp()
+        self.assertTrue((0, 0) == self.m.pos2index(((-(SIZE / 2 - BLOCKSIZE)), (SIZE / 2 - BLOCKSIZE))), f"{self.m.pos2index(((-(SIZE / 2 - BLOCKSIZE)), (SIZE / 2 - BLOCKSIZE)))}")
+        self.assertTrue(((SIZE / BLOCKSIZE - 1), (SIZE / BLOCKSIZE - 1)) == self.m.pos2index(((SIZE / 2), (-(SIZE / 2)))), f"{self.m.pos2index(((SIZE / 2), (-(SIZE / 2))))}")
+
+    def testSettingMatrixValues(self):
+        self.m.reset()
+        value = self.m.getMatrixValueAt(self.m.turtle.position())
+        self.assertTrue(0 == value)
+        value = 1
+        self.m.setMatrixValueAt(self.m.turtle.position(), value)
+        self.assertEqual(self.m.matrix[0][0], 1)
+        self.m.turtle.goto(-(SIZE / 2 - 2 * BLOCKSIZE), SIZE / 2 - BLOCKSIZE)
+        value = self.m.getMatrixValueAt(self.m.turtle.position())
+        self.assertTrue(1 == value)
+        value = 0
+        self.m.setMatrixValueAt(self.m.turtle.position(), value)
+        self.assertEqual(self.m.matrix[0][1], 0)
+        self.m.turtle.goto(SIZE / 2, -(SIZE / 2))
+        value = self.m.getMatrixValueAt(self.m.turtle.position())
+        self.assertTrue(1 == value)
+        value = 0
+        self.m.setMatrixValueAt(self.m.turtle.position(), value)
+        self.assertTrue(0 == value)
+
+    def testSetMatrixValueAt(self):
+        self.m.setMatrixValueAt(self.m.turtle.pos(), 1)
+        self.assertTrue(self.m.matrix[0][0] == 1)
+        self.m.setMatrixValueAt(self.m.turtle.pos(), 0)
+        self.assertTrue(self.m.matrix[0][0] == 0)
+
+    def testDig(self):
+        self.m.dig(EAST)
+        self.assertEqual(self.m.turtle.pos(), (-(SIZE / 2 - 2 * BLOCKSIZE), SIZE / 2 - BLOCKSIZE))
+        self.assertTrue(self.m.matrix[1][0] == 0)
+        self.m.dig(WEST)
+        self.assertEqual(self.m.turtle.pos(), (-(SIZE / 2 - BLOCKSIZE), SIZE / 2 - BLOCKSIZE))
+        self.m.dig(NORTH)
+        self.assertEqual(self.m.turtle.pos(), (-(SIZE / 2 - BLOCKSIZE), SIZE / 2 - BLOCKSIZE))
+        self.m.dig(SOUTH)
+        self.assertEqual(self.m.turtle.pos(), (-(SIZE / 2 - BLOCKSIZE), SIZE / 2 - 2 * BLOCKSIZE))
+
+    def testBreakThrough(self):
+        self.m.dig(EAST)
+        self.m.dig(EAST)
+        self.m.dig(EAST)
+        self.m.dig(EAST)
+        self.m.dig(SOUTH)
+        self.m.dig(SOUTH)
+        self.m.dig(WEST)
+        self.m.dig(WEST)
+        self.m.dig(NORTH)
+        self.assertTrue(self.m.turtle.pos() == (-(SIZE / 2 - 3 * BLOCKSIZE), SIZE / 2 - 3 * BLOCKSIZE), f"{self.m.turtle.pos()}")
+
+    def testBreakThrough2(self):
+        self.m.dig(SOUTH)
+        self.m.dig(SOUTH)
+        self.m.dig(SOUTH)
+        self.m.dig(SOUTH)
+        self.m.dig(EAST)
+        self.m.dig(EAST)
+        self.m.dig(NORTH)
+        self.m.dig(NORTH)
+        self.m.dig(WEST)
+        self.assertTrue(self.m.turtle.pos() == (-(SIZE / 2 - 3 * BLOCKSIZE), SIZE / 2 - 3 * BLOCKSIZE), f"{self.m.turtle.pos()}")
+
+    def testNeighbors(self):
+        self.assertTrue(self.m.neighbors() == [(1, 0), (-1, 1), (-1, 2), (1, 3)], f"{self.m.neighbors()}")
+        r = self.m.neighbors()
+        self.assertTrue((r[0] == (1, 0) and r[1] == (-1, 1) and
+                         r[2] == (-1, 2) and r[3] == (1, 3)), "got " + str(r))
+        self.m.reset()
+        self.m.turtle.goto(-(SIZE / 2 - 2 * BLOCKSIZE), SIZE / 2 - 2 * BLOCKSIZE)
+        r = self.m.neighbors()
+        self.assertTrue((r[0] == (1, 0) and r[1] == (-1, 1) and
+                         r[2] == (-1, 2) and r[3] == (1, 3)), "got " + str(r))
+        self.m.reset()
+        self.m.turtle.goto(-(SIZE / 2 - 3 * BLOCKSIZE), SIZE / 2 - 3 * BLOCKSIZE)
+        r = self.m.neighbors()
+        self.assertTrue((r[0] == (1, 0) and r[1] == (1, 1) and
+                         r[2] == (1, 2) and r[3] == (1, 3)), "got " + str(r))
+
+
+if __name__ == "__main__":
+    unittest.main()
